4831fc718a9e33f5ae54175de54dd17eae3aceef
Navneet Singh Gill <navneet105gill@gmail.com>
2021-12-16 01:46:01 -0500

    add production config

--- utility/log.js
+++ utility/log.js
@@ -2,10 +2,12 @@ const colors = require('colors');
 
 const success = (message) => {
     console.log(colors.green(message).inverse);
+    // console.log("Succes: " + message);
 };
 
 const error = (message) => {
     console.log(colors.red(message).inverse);
+    // console.log("Error: " + message);
 };
 
 module.exports = {
